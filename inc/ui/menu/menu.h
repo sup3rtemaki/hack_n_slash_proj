ec9736ed20fcdc6546f4310f9b9ac3026a1e1d76
Celso Junior <celsofer93@gmail.com>
2024-11-04 21:24:42 -0300

    Creates Menu class pt2

--- inc/ui/menu/menu.h
+++ inc/ui/menu/menu.h
@@ -6,8 +6,17 @@ class Menu : public Ui {
 public:
 	Menu(class Hero* hero);
 
+	int index;
+
 	virtual void draw() override;
 	virtual void setUp() override;
+
+private:
+	Hero* hero = nullptr;
+
+	void drawMenuBackground();
+	void drawText();
+	void drawSelectionBox();
 };
 
 #endif // !MENU
--- src/ui/menu/menu.cpp
+++ src/ui/menu/menu.cpp
@@ -1,1 +1,20 @@
 #include "ui/menu/menu.h"
+
+Menu::Menu(Hero* hero) {
+	index = 0;
+}
+
+void Menu::draw() {
+}
+
+void Menu::setUp() {
+}
+
+void Menu::drawMenuBackground() {
+}
+
+void Menu::drawText() {
+}
+
+void Menu::drawSelectionBox() {
+}
